0c53ca3b27da334e4f8cb2037a28511682c388e2
Elias <elias@mycarly.com>
2024-12-09 13:47:05 +0100

    Add Refelection Helper

---
+++ Helper/ReflectionHelper.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+using System.Runtime;
+
+namespace Beancounter.Helper;
+
+public static class ReflectionHelper
+{
+    
+    public static IEnumerable<Type> FindDerivedClasses<T>()
+    {
+        var baseType = typeof(T);
+        var assembly = Assembly.GetAssembly(baseType);
+        if (assembly == null) return [];
+        var types = assembly.GetTypes();
+        return types
+            .Where(t => t is { IsClass: true } && t.IsSubclassOf(baseType));
+    }
+
+    public static IEnumerable<Type> FindImplementingClasses<T>()
+    {
+        var baseType = typeof(T);
+        var assembly = Assembly.GetAssembly(baseType);
+        if (assembly == null) return [];
+        var types = assembly.GetTypes();
+        return types
+            .Where(t => t is { IsClass: true } && t.GetInterfaces().Contains(baseType));
+    }
+
+}
