d5ee0f1dda3d3a5fdd68c4184d707715441f3293
Elias <elias@mycarly.com>
2025-09-03 11:10:51 +0200

    Adds string method to generate ID slugs
    Provides a utility to transform human-readable strings into a clean, machine-friendly identifier format.
    
    Normalizes input by converting to lowercase, replacing spaces and special characters with underscores, and consolidating multiple underscores. Ensures consistent and safe identifiers for various system uses.

--- Extension/String+Extension.cs
+++ Extension/String+Extension.cs
@@ -1,5 +1,6 @@
 using System.Globalization;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Beancounter.Extension;
 
@@ -26,5 +27,17 @@ public static class String_Extension {
 
         // Normalize back to FormC
         return sb.ToString().Normalize(NormalizationForm.FormC);
+    }
+
+    public static string ToStringId(this string input) {
+        var result = input;
+        result = result.Replace(" ", "_");
+        result = result.ToLower();
+        result = result.ConvertToAscii();
+        // Replace non a-zA-Z0-9 with _
+        result = Regex.Replace(result, @"[^a-zA-Z0-9]", "_");
+        result = Regex.Replace(result, @"_+", "_");
+        result = result.TrimEnd('_');
+        return result;
     }
 }
